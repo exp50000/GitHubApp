ef4198a61c9787703f4b2a4292deab63c35b258e
Ric <exp50000@gmail.com>
2020-05-23 16:50:36 +0800

    Project initialization

---
+++ GitHubApp/ViewController/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  GitHubApp
+//
+//  Created by Ric. on 2020/5/23.
+//  Copyright © 2020 Ric. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
